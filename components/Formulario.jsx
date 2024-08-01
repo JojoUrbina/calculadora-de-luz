edf5cef101d11f36586e5ebab221aafe6aa31946
Jorman Urbina <jormanurbina1@gmail.com>
2024-08-01 13:09:34 +0200

    Se ajustan estilos

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -178,6 +178,7 @@ const Formulario = ({ setResultadoMensual }) => {
       <Button
         id="btn-calcular"
         variant="contained"
+        
         startIcon={
           <CalculateIcon
             sx={{
